471873d1e694988061303f843c6b5388191b2093
Tonight <96319418+over-tonight@users.noreply.github.com>
2025-04-23 16:58:36 +0100

    Main file.

---
+++ index.py
@@ -0,0 +1,29 @@
+import requests
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix="/", intents= discord.Intents.all())
+
+TOKEN = '' # Put your discord token here.
+
+# Dont edit anything below unless you know what you're doing.
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    await client.tree.sync()
+
+@client.tree.command()
+async def search(interaction: discord.Interaction, user: str):
+    channel = interaction.channel
+    req = requests.get(f"https://inventory.roblox.com/v1/users/{user}/items/1/961333085/is-owned")
+    print(req.status_code)
+    if req.status_code == 200:
+        # Found gamepass message
+        await interaction.response.send_message(embed=discord.Embed(title=f"Found gamepass!", description="Confirmed", color=0x00ff00))
+    else:
+        # Couldnt find gamepass message
+        await interaction.response.send_message(embed=discord.Embed(title=f"Failed to find gamepass.", description="Did you make your inventory public?", color=0xff0000))
+        
+client.run(TOKEN)
